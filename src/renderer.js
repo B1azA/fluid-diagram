5493de0fd69574c24cbb24f4f3abf873535d0f10
B1azA <77540263+B1azA@users.noreply.github.com>
2022-03-19 16:30:07 +0100

    is_mouse_on() fixed

--- src/renderer.js
+++ src/renderer.js
@@ -15,31 +15,43 @@ class Block {
     }
 
     draw() {
+        
+        var rec_pos = new Point(this.pos.x * scale + offset.x, this.pos.y * scale + offset.y);
+        var width = this.fullsize.x;
+        var height = this.fullsize.y;
+        canvas.draw_rectangle("#FFFFFF", rec_pos, width, height);
+
         this.fullsize.y = 0;
         var pos = new Point(this.pos.x * scale, this.pos.y * scale);
+        var max_fullsizex = 0;
         this.line.forEach(line => {
             var size = canvas.measure_text(line, this.size * scale);
             canvas.draw_text_background(line, "#000000", this.color, 
                 new Point(pos.x + offset.x, pos.y + offset.y), this.size * scale);
             var fulls = canvas.measure_text_background(line, this.size * scale);
-            var fullsize = new Point(fulls.x * scale, fulls.y * scale);
+            var fullsize = new Point(fulls.x, fulls.y);
             pos.y += size.y;
-            this.fullsize.y += size.y * scale;
+            this.fullsize.y += size.y;
             
-            if (fullsize.x > this.fullsize.x) {
+            if (fullsize.x > max_fullsizex) {
                 this.fullsize.x = fullsize.x;
+                max_fullsizex = fullsize.x;
             }
         });
     }
 
     is_mouse_on() {
-        var left_side = this.pos.x + offset.x * scale;
-        var right_side = this.pos.x + this.fullsize.x + offset.x * scale;
-        var top_side = this.pos.y + offset.y * scale;
-        var bottom_side = this.pos.y + this.fullsize.y + offset.y * scale;
+        var rec_pos = new Point(this.pos.x * scale + offset.x, this.pos.y * scale + offset.y);
+        var width = this.fullsize.x;
+        var height = this.fullsize.y;
 
-        if (scaled_mouse_pos.x > left_side && scaled_mouse_pos.x < right_side && 
-            scaled_mouse_pos.y - 1 > top_side && scaled_mouse_pos.y < bottom_side) {
+        var left_side = rec_pos.x;
+        var right_side = rec_pos.x + width;
+        var top_side = rec_pos.y;
+        var bottom_side = rec_pos.y + height;
+
+        if (mouse_pos.x > left_side && mouse_pos.x < right_side && 
+            mouse_pos.y - 1 > top_side && mouse_pos.y < bottom_side) {
             return true;
         }
         return false;
@@ -63,10 +75,13 @@ function draw() {
     canvas.draw_text("scale: " + scale.toFixed(2), "white",
         new Point(canvas.get_canvas_size().x - canvas.measure_text("scale: " + scale.toFixed(2) + "00", 20).x, 
             canvas.measure_text("scale: " + scale.toFixed(2), 20).y), 20);
+    canvas.draw_text("m_pos: " + mouse_pos.x.toFixed(2) + "; " + mouse_pos.y.toFixed(2), "white",
+        new Point(canvas.get_canvas_size().x -
+            canvas.measure_text("m_pos: " + mouse_pos.x.toFixed(2) + "; " + mouse_pos.y.toFixed(2) + "00", 20).x,
+            canvas.measure_text("m_pos: " + mouse_pos.x.toFixed(2) + "; " + mouse_pos.y.toFixed(2), 20).y + 20), 20);
 }
 
 var mouse_pos = canvas.get_mouse_position();
-var scaled_mouse_pos = new Point(0, 0);
 var last_mouse_pos = new Point(0, 0);
 var mouse_delta = new Point(0, 0);
 var wheel = canvas.get_mouse_wheel();
@@ -74,7 +89,6 @@ var last_wheel = 0;
 var wheel_delta = 0;
 function update() {
     mouse_pos = canvas.get_mouse_position();
-    scaled_mouse_pos = new Point(mouse_pos.x * scale, mouse_pos.y * scale);
     mouse_delta = new Point(mouse_pos.x - last_mouse_pos.x, mouse_pos.y - last_mouse_pos.y);
     last_mouse_pos = mouse_pos;
     wheel = canvas.get_mouse_wheel()
